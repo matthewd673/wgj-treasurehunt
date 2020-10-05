aff18aa23f599ec61f5cd39b41618fcfc8b37f6d
Matthew Daly <matthewd673@gmail.com>
2020-10-04 23:05:09 -0400

    Add tile and character sprites

--- src/com/mattdaly/wgj/treasurehunt/Sprites.java
+++ src/com/mattdaly/wgj/treasurehunt/Sprites.java
@@ -12,11 +12,27 @@ public final class Sprites {
 	//list of all sprites
 	public static BufferedImage frog;
 	
+	//characters
+	public static BufferedImage drill;
+	
+	//tiles
+	public static BufferedImage background;
+	public static BufferedImage dirt;
+	public static BufferedImage toughDirt;
+	public static BufferedImage treasure;
+	
 	public static void loadSprites() {
 		
 		try {
 			//load sprite from file
 			frog = ImageIO.read(new File("res/frog.png"));
+			
+			drill = ImageIO.read(new File("res/drill.png"));
+			
+			background = ImageIO.read(new File("res/background.png"));
+			dirt = ImageIO.read(new File("res/dirt.png"));
+			toughDirt = ImageIO.read(new File("res/tough-dirt.png"));
+			treasure = ImageIO.read(new File("res/treasure.png"));
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
